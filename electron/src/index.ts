ee74690d3d18509a6a57e6ce854ed168844b365e
Karthik R <karthikeya.rachamolla@gmail.com>
2026-01-26 17:14:06 +0530

    adding frame para to window

--- electron/src/index.ts
+++ electron/src/index.ts
@@ -13,8 +13,9 @@ if (require('electron-squirrel-startup')) {
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
+    height: 400,
     width: 800,
+    frame: true,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },

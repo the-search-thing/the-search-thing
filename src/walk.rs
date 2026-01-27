33395f6e090f7f8007b10341344e73d6c09d31ea
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-27 08:32:57 -0500

    add integrate read file content into walk, rename to walkand get content

--- src/walk.rs
+++ src/walk.rs
@@ -1,12 +1,15 @@
+use crate::read_file::get_file_contents;
 use pyo3::prelude::*;
 use walkdir::WalkDir;
-
 #[pyfunction]
-pub fn walk(dir: String) -> PyResult<()> {
+pub fn walk_and_get_content(dir: String) -> PyResult<()> {
     for entry in WalkDir::new(dir) {
         let entry =
             entry.map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{}", e)))?;
-        println!("{}", entry.path().display());
+        if entry.file_type().is_file() {
+            let path_str = entry.path().to_string_lossy().to_string();
+            get_file_contents(path_str);
+        }
     }
     Ok(())
 }

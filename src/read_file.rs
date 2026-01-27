55c6ebdc8145a9b08f621dea9eb6de65d3b204bd
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-27 08:32:24 -0500

    min. reformat

--- src/read_file.rs
+++ src/read_file.rs
@@ -4,11 +4,9 @@ use std::fs;
 use pyo3::prelude::*;
 #[pyfunction]
 pub fn get_file_contents(file_path: String) {
-    // let args: Vec<String> = env::args().collect();
-    // let query = &args[1];
     let file_path = file_path;
 
-    println!("in file {file_path}");
-    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
+    println!("File Path: {file_path}");
+    let contents = fs::read_to_string(file_path).expect("shoulda been able to read the file");
     println!("with text:\n{contents}")
 }

fb4884d0e99d8f3a6af06e0380d734ee4e14a021
Karthik R <karthikeya.rachamolla@gmail.com>
2026-01-31 15:47:16 +0530

    adding nofiles svg

--- electron-react-app-main/app/components/Results.tsx
+++ electron-react-app-main/app/components/Results.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { useAppContext } from "./AppContext"
+import noFiles from "@/resources/no-files-found.svg"
 
 interface ResultsProps {
   results: string[]
@@ -27,8 +28,9 @@ export default function Results({ results, query }: ResultsProps) {
   return (
     <div className="flex items-center justify-center w-full h-full">
       {!isIndexed ? (
-        <div className="flex items-center justify-center w-full h-full">
-          <div className="text-zinc-500">No files have been indexed yet...</div>
+        <div className="flex flex-col items-center justify-center gap-4 w-full h-full pt-30">
+          <img src={noFiles} alt="No files" className="w-15 h-15 opacity-75" />
+          <div className="text-zinc-500">No files have been indexed :(</div>
         </div>
       ) : (
         <div className="flex flex-col items-center justify-center w-full h-full">

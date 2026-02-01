d3e6b29a2a0e1746f70026424ef22df18c25a05f
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-31 20:12:42 -0500

    add new func walk and get files conetn

--- src/walk.rs
+++ src/walk.rs
@@ -1,5 +1,6 @@
-// use crate::read_file::get_file_contents;
+use crate::read_file::get_file_contents;
 use pyo3::prelude::*;
+use std::collections::HashMap;
 use walkdir::WalkDir;
 
 #[pyfunction]
@@ -11,4 +12,23 @@ pub fn walk_and_get_files(dir: String) -> PyResult<Vec<String>> {
         paths.push(entry.path().to_string_lossy().to_string());
     }
     Ok(paths)
+}
+#[pyfunction]
+pub fn walk_and_get_files_content(dir: String) -> PyResult<HashMap<String, String>> {
+    let mut files_content = HashMap::new();
+    for entry in WalkDir::new(dir) {
+        let entry =
+            entry.map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{}", e)))?;
+        let path = entry.path().to_string_lossy().to_string();
+
+        if entry.path().is_file() {
+            match get_file_contents(path.clone()) {
+                Ok(content) => {
+                    files_content.insert(path, content);
+                }
+                Err(_) => continue,
+            }
+        }
+    }
+    Ok(files_content)
 }

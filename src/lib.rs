17266e0fc8feb5fddc82f625067a95984f39b31c
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-31 20:12:20 -0500

    add walk and get file content  func

--- src/lib.rs
+++ src/lib.rs
@@ -15,5 +15,6 @@ fn the_search_thing(m: &Bound<'_, PyModule>) -> PyResult<()> {
         m
     )?)?;
     m.add_function(wrap_pyfunction!(read_file::get_file_contents, m)?)?;
+    m.add_function(wrap_pyfunction!(walk::walk_and_get_files_content, m)?)?;
     Ok(())
 }

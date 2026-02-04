4aa7f18fcede12e90daa0f2cd811e1ff8739f859
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-02-04 18:28:05 -0500

    mod aud expose detect-tempo

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use pyo3::prelude::*;
+mod aud;
 mod filetype_check;
 mod helpers;
 mod index;
 mod read_file;
 mod vid;
 mod walk;
-
 #[pymodule]
 fn the_search_thing(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(index::rust_indexer, m)?)?;
@@ -17,5 +17,6 @@ fn the_search_thing(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(read_file::get_file_contents, m)?)?;
     m.add_function(wrap_pyfunction!(walk::walk_and_get_files_content, m)?)?;
     m.add_function(wrap_pyfunction!(walk::walk_and_get_text_file_batch, m)?)?;
+    m.add_function(wrap_pyfunction!(aud::detect_tempo, m)?)?;
     Ok(())
 }

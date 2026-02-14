7c426cce8d7125d358f2ae478fa073f40c0aaa77
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-02-14 13:56:10 +0530

    comment out validate file exitsts
    Co-authored-by: Karthik R <karthikeya.rachamolla@gmail.com>

--- src/img.rs
+++ src/img.rs
@@ -1,6 +1,4 @@
-use crate::helpers::{
-    validate_file_exists
-};
+// use crate::helpers::validate_file_exists;
 use base64::engine::general_purpose::STANDARD;
 use base64::Engine;
 use image::ImageReader;
@@ -9,7 +7,7 @@ use pyo3::{PyErr, PyResult};
 
 #[pyfunction]
 pub fn get_base64_bytes(image_path: String) -> PyResult<String> {
-    validate_file_exists(&image_path);
+    //  validate_file_exists(&image_path);
     let img = ImageReader::open(&image_path)
         .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?
         .decode()

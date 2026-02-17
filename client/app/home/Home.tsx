ef52f163b3f68fdf81d8da4aafe8012c54fe1ae4
Karthik R <karthikeya.rachamolla@gmail.com>
2026-02-17 12:35:45 +0530

    adding hasInteracted state for welcome ui

--- client/app/home/Home.tsx
+++ client/app/home/Home.tsx
@@ -14,9 +14,11 @@ export default function Home() {
   const [hasSearched, setHasSearched] = useState(false) //temporary logic (pls remove in the future :pray:)
   const [isLoading, setIsLoading] = useState(false)
   const [awaitingIndexing, setAwaitingIndexing] = useState(false)
+  const [hasInteracted, setHasInteracted] = useState(false)
 
   const handleSearch = async () => {
     const lastResultsEmpty = (searchResults?.results?.length ?? 0) === 0
+    setHasInteracted(true)
 
     if (hasSearched && lastResultsEmpty) {
       setAwaitingIndexing(true)
@@ -44,6 +46,7 @@ export default function Home() {
             setQuery(e.target.value)
             setHasSearched(false)
             setAwaitingIndexing(false)
+            setHasInteracted(true)
           }}
           placeholder="Search for files or folders…"
           onKeyDown={(e) => {
@@ -55,25 +58,30 @@ export default function Home() {
         />
       </div>
 
-      <div
-        className={cn(
-          'flex flex-1 min-h-0',
-          'border-2 border-zinc-700/80 bg-zinc-800/60',
-          'px-4 shadow-[0_0_0_1px_rgba(255,255,255,0.03)]'
-        )}
-      >
-        {isLoading ? (
-          <div className="flex items-center justify-center w-full text-zinc-400">Searching...</div>
-        ) : (
-          <Results
-            searchResults={searchResults}
-            query={query}
-            hasSearched={hasSearched}
-            awaitingIndexing={awaitingIndexing}
-            onIndexingCancelled={() => setAwaitingIndexing(false)}
-          />
-        )}
-      </div>
+      
+      {hasInteracted ? (
+        <div
+          className={cn(
+            'flex flex-1 min-h-0',
+            'border-2 border-zinc-700/80 bg-zinc-800/60',
+            'px-4 shadow-[0_0_0_1px_rgba(255,255,255,0.03)]'
+          )}
+        >
+          {isLoading ? (
+            <div className="flex items-center justify-center w-full text-zinc-400">Searching...</div>
+          ) : (
+            <Results
+              searchResults={searchResults}
+              query={query}
+              hasSearched={hasSearched}
+              awaitingIndexing={awaitingIndexing}
+              onIndexingCancelled={() => setAwaitingIndexing(false)}
+            />
+          )}
+        </div>
+      ) : (
+        
+      )}
 
       <div
         className={cn(

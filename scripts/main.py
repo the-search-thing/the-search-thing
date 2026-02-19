06927bb81f15860ef55d95c167f9257802c403e4
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-02-19 11:56:36 +0530

    remove(backend/scripts): delete dead code/scripts

--- scripts/main.py
+++
@@ -1,7 +0,0 @@
-from the_search_thing import walk_and_get_files_content
-
-files_with_content = walk_and_get_files_content(
-    "C:\\Users\\amaan\\OneDrive\\Documents\\coding\\the-search-thing\\src"
-)
-for path, content in files_with_content.items():
-    print(f"{path}: {content}")

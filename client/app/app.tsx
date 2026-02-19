bebd63b885fab0b95710746199ffe6f37341ab9c
Karthik R <karthikeya.rachamolla@gmail.com>
2026-02-19 13:12:36 +0530

    add(/app): adding routes to app.tsx

--- client/app/app.tsx
+++ client/app/app.tsx
@@ -1,11 +1,18 @@
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import Home from '@/app/home/Home'
+import Settings from '@/app/settings/Settings'
 import './styles/app.css'
 import { AppProvider } from './AppContext'
 
 export default function App() {
   return (
     <AppProvider>
-      <Home />
+      <MemoryRouter initialEntries={['/']} initialIndex={0}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </MemoryRouter>
     </AppProvider>
   )
 }

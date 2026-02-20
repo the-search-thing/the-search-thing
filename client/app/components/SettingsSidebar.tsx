8533099754e389c57e7b6bd996de983b396655ad
Karthik R <karthikeya.rachamolla@gmail.com>
2026-02-20 13:19:02 +0530

    add(SettingsSidebar): props with selectedItem and onSelect

--- client/app/components/SettingsSidebar.tsx
+++ client/app/components/SettingsSidebar.tsx
@@ -2,13 +2,19 @@ import { cn } from '@/lib/utils'
 
 const items = ['General', 'About', 'Keybinds'] as const
 
-export default function SettingsSidebar() {
+type SettingsSideBarProps = {
+  selectedItem: string
+  onSelect: (item: string) => void
+}
+
+export default function SettingsSidebar({selectedItem, onSelect}: SettingsSideBarProps) {
+  
   return (
     <div
       className={cn(
         'flex flex-col gap-2',
         'w-56 flex-none',
-        'border-2 border-zinc-700/80 bg-zinc-800/60',
+        'border-1 border-zinc-700/80 bg-zinc-800/60',
         'p-3 shadow-[0_0_0_1px_rgba(255,255,255,0.03)]'
       )}
     >
@@ -18,10 +24,11 @@ export default function SettingsSidebar() {
           <button
             key={label}
             type="button"
+            onClick={() => onSelect(label)}
             className={cn(
               'flex items-center justify-start',
               'h-9 w-full rounded-md px-2',
-              'text-zinc-300 hover:text-zinc-100',
+              selectedItem === label ? 'bg-zinc-700/60 text-zinc-100' : 'text-zinc-300 hover:text-zinc-100',
               'hover:bg-zinc-700/60',
               'transition-colors duration-150'
             )}

a93f8947d01567708165f94acbd4285fe3a398e6
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-02-28 14:39:44 -0600

    about page

---
+++ website/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+
+import { FAQ } from "@/components/FAQ";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col pl-20 pr-20 text-center items-center font-sans-code">
+
+    <h1 className="font-bold">
+    Our vision</h1>
+<p>
+We're building the-search-thing, a small two person team with a vision for building the world's best search tool for you, and your OS.</p>
+
+    </main>)}
